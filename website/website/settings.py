bbfa7ab9e1b470662ceed05e4d4c5a64c4425184
itsamirali <57327052+itsamirali@users.noreply.github.com>
2023-06-06 19:12:54 +0330

    added user model and change default user model in settings

--- website/website/settings.py
+++ website/website/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'crispy_bootstrap5'
+    'rest_framework',
+    'user'
 ]
 
 MIDDLEWARE = [
@@ -122,3 +123,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'user.User'
