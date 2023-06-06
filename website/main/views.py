cea962b5af28f7e9acbd19db4bbd97e7932e580a
itsamirali <57327052+itsamirali@users.noreply.github.com>
2023-06-06 18:17:25 +0330

    added base

--- website/main/views.py
+++ website/main/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def home(request):
+    method = request.method
+    return HttpResponse("the request method is " + method)
